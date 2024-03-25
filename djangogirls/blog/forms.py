4cf57429d837e99cf4842daf6ea6ab5fbcf6c875
Whitelatern <iamlatern@gmail.com>
2024-03-25 11:42:59 +0530

    Added views to create/edit blog post ings.

--- djangogirls/blog/forms.py
+++ djangogirls/blog/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment, Review
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ('author', 'text',)
